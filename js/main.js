776e8435ab5c122c002a3ac05c33a6ec20482560
Your Name <your.email@example.com>
2025-11-17 15:02:13 +0530

    html css and JS updated

--- js/main.js
+++ js/main.js
@@ -233,3 +233,11 @@
   }
 
 })(); // IIFE end
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  const isOpen = navMenu.classList.toggle("show");
+  hamburger.classList.toggle("active", isOpen);
+});
